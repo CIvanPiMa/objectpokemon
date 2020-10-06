7ca65a972685b32b9faf95d392aa71cacb099869
Cesar Ivan Piña Martinez <cesar.pina@cesarpina.local>
2020-10-06 14:39:08 -0500

    Chocolob OP final

--- chocolob.py
+++ chocolob.py
@@ -5,9 +5,9 @@ class Pokemon(BasePokemon):
     def __init__(self):
         BasePokemon.__init__(self)
         # Has to sum to 100
-        self.spend_hp(50)
-        self.spend_attack(50)
-        self.spend_defence(0)
+        self.spend_hp(20)
+        self.spend_attack(55)
+        self.spend_defence(25)
         self.add_move(Echatelo())
         self.set_type(Type.NORMAL)
 
@@ -26,13 +26,3 @@ class Echatelo(BaseMove):
 
     def get_name(self):
         return "Echatelo"
-
-
-class Cachetada(BaseMove):
-    def __init__(self):
-        BaseMove.__init__(self)
-        self.choose_uses(5)
-        self.set_type(Type.NORMAL)
-
-    def get_name(self):
-        return "Cachetada"
